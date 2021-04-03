94cefcad0d91ac9d073b180c83d3963a4f6f9b63
Riya_2411 <riyanchhadva@gmail.com>
2021-04-03 23:54:02 +0530

    add: IPADResponsivenessCommit

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -18,15 +18,15 @@ class Signup extends React.Component{
                         </div>
                         <form className="Signup__Form">
                             <div className="form-row Signup__dmrow">
-                            <div className="col-lg-4 col-xs-12 col-sm-12">
+                            <div className="col-lg-4 col-xs-12 col-sm-4">
                                 <input type="text" className="form-control" title="*ENTER CHARACTER VALUES ONLY" pattern="[A-Za-z]{1,32}" name="studentFirstName" id="studentFirstName" placeholder=" " required />
                                 <span>First Name</span>
                             </div>
-                            <div className="col-lg-4">
+                            <div className="col-lg-4 col-sm-4">
                                     <input type="text" className="form-control" title="*ENTER CHARACTER VALUES ONLY" pattern="[A-Za-z]{1,32}" name="studentMiddleName" id="studentMiddleName" placeholder=" " required />
                                     <span>Middle Name</span>
                             </div>
-                            <div className="col-lg-4">
+                            <div className="col-lg-4 col-sm-4">
                                     <input type="text" className="form-control" title="*ENTER CHARACTER VALUES ONLY" pattern="[A-Za-z]{1,32}" name="studentLastName" id="studentLastName" placeholder=" " required />
                                     <span>Last Name</span>
                                 </div>
@@ -47,15 +47,15 @@ class Signup extends React.Component{
                             </div> */}
 
                             <div className="form-row Signup__dmrow">
-                                <div className="col-lg-4">
+                                <div className="col-lg-4 col-sm-4">
                                     <input type="tel" title="*NUMBER SHOULD BE OF 10 DIGITS ONLY" pattern="[1-9]{1}[0-9]{9}" name="studentContactNo" id="studentContactNo" className="form-control" placeholder=" " required />
                                     <span>Mobile Number</span>
                                 </div>
-                                <div className="col-lg-4">
+                                <div className="col-lg-4 col-sm-4">
                                     <input type="tel" className="form-control" name="studentAlternateContactNo" id="studentAlternateContactNo" title="*NUMBER SHOULD BE OF 10 DIGITS ONLY" pattern="[1-9]{1}[0-9]{9}" placeholder=" " required />
                                     <span>Alternate Mobile Number</span>
                                 </div>
-                                <div className="col-lg-4">
+                                <div className="col-lg-4 col-sm-4">
                                     <input type="email" className="studentEmailId" id="studentEmailId" className="form-control" placeholder=" " required  />
                                     <span>Email Address</span>
                                 </div>
@@ -76,11 +76,11 @@ class Signup extends React.Component{
                             </div> */}
 
                             <div className="form-row Signup__dmrow">
-                                <div className="col-lg-4">
+                                <div className="col-lg-4 col-sm-4">
                                     <input type="password" className="studentPassword" id="studentPassword" className="form-control" placeholder=" " required  />
                                     <span>Enter Password</span>
                                 </div>
-                                <div className="col-lg-4">
+                                <div className="col-lg-4 col-sm-4">
                                     <input type="password" className="studentConfirmPassword" id="studentConfirmPassword" className="form-control" placeholder=" " required  />
                                     <span>Confirm Password</span>
                                 </div>
